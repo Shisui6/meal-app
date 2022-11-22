e550915cd10a2f696f83e0b6dc9f231c335412db
Shisui <37116322+Shisui6@users.noreply.github.com>
2022-11-22 11:58:21 +0100

    Create navbar

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 // Imports
 import './style.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import logoImg from './images/logo.png';
+import profileImg from './images/profile.png';
+
+const logo = document.getElementById('logo');
+const profile = document.getElementById('profile');
+
+logo.src = logoImg;
+profile.src = profileImg;
