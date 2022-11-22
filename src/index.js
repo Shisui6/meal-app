28d5b2d23ca489efc306f647045d4fc43056172a
Shisui <37116322+Shisui6@users.noreply.github.com>
2022-11-22 21:49:05 +0100

    Implement loading screens

--- src/index.js
+++ src/index.js
@@ -11,33 +11,35 @@ logo.src = logoImg;
 profile.src = profileImg;
 
 // Get relevant elements from the DOM
-const main = document.getElementById('main');
+const meals = document.getElementById('meals-id');
 const sidebar = document.getElementById('sidebar-id');
+const loaderMain = document.getElementById('skeleton-loader-main');
+const loaderSide = document.getElementById('skeleton-loader-side');
 
-const fetchMealsByCategory = async () => {
+const fetchMealsByCategory = async (cat) => {
   try {
-    const response = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Pasta');
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${cat}`);
     if (response.ok) {
       const json = await response.json();
-      json.meals.splice(1, 1);
+      loaderMain.classList.toggle('hide');
       json.meals.forEach((item) => {
         const mealElem = document.createElement('div');
         mealElem.className = 'meal';
-        mealElem.innerHTML = `
-          <img src="${item.strMealThumb}" alt="meal">
-          <h3>${item.strMeal}</h3>
-          <div class="meal-info">
-            <div class="comments">
-              <i class="bi bi-chat"></i>
-              <p>50</p>
-            </div>
-            <div class="likes">
-              <i class="bi bi-heart"></i>
-              <p>100</p>
-            </div>
+        mealElem.insertAdjacentHTML('beforeend', `
+        <img src="${item.strMealThumb}" alt="meal">
+        <h3>${item.strMeal}</h3>
+        <div class="meal-info">
+          <div class="comments">
+            <i class="bi bi-chat"></i>
+            <p>50</p>
           </div>
-        `;
-        main.appendChild(mealElem);
+          <div class="likes">
+            <i class="bi bi-heart"></i>
+            <p>100</p>
+          </div>
+        </div>
+      `);
+        meals.appendChild(mealElem);
       });
     }
   } catch (error) {
@@ -54,13 +56,30 @@ const fetchCategories = async () => {
       json.categories.splice(4, 1);
       json.categories.splice(5, 4);
       json.categories.pop();
+      loaderSide.classList.toggle('hide');
       json.categories.forEach((item) => {
         const categoryElem = document.createElement('div');
-        categoryElem.innerHTML = `
-          <img src="${item.strCategoryThumb}" alt="meal">
-          <p>${item.strCategory}</p>
-        `;
+        categoryElem.className = 'cat';
+        if (item.strCategory === 'Pasta') {
+          categoryElem.classList.add('selected');
+        }
+        categoryElem.id = `category-${item.strCategory}`;
+        categoryElem.insertAdjacentHTML('beforeend', `
+        <img src="${item.strCategoryThumb}" alt="meal">
+        <p>${item.strCategory}</p>
+      `);
         sidebar.appendChild(categoryElem);
+
+        document.getElementById(`category-${item.strCategory}`).addEventListener('click', () => {
+          meals.innerHTML = '';
+          loaderMain.classList.toggle('hide');
+          const cats = document.querySelectorAll('#sidebar-id div');
+          for (let i = 0; i < cats.length; i += 1) {
+            cats[i].classList.remove('selected');
+          }
+          categoryElem.classList.add('selected');
+          fetchMealsByCategory(item.strCategory);
+        });
       });
     }
   } catch (error) {
@@ -68,5 +87,7 @@ const fetchCategories = async () => {
   }
 };
 
+loaderMain.classList.toggle('hide');
+loaderSide.classList.toggle('hide');
 fetchCategories();
-fetchMealsByCategory();
+fetchMealsByCategory('Pasta');
