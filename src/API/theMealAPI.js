8b353de1bfdaf29e6afd01ce3f8e7db6d4a9c72f
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2022-11-23 16:22:25 +0300

    comments for my code

--- src/API/theMealAPI.js
+++ src/API/theMealAPI.js
@@ -2,18 +2,21 @@ const invo = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi
 const mealSearch = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
 const key = 'ZL01BQpf9fh0l4EnAOtP';
 
+// geting a meal by the id of it
 const getById = async (id) => {
   const response = await fetch(`${mealSearch}${id}`);
   const result = await response.json();
   return result;
 };
 
+// geting a comments by the id of the meal
 const getComments = async (id) => {
   const response = await fetch(`${invo}${key}/comments?item_id=${id}`);
   const result = await response.json();
   return result;
 };
 
+// adding a coments
 const addComments = async (newScore) => {
   await fetch(`${invo}${key}/comments`, {
     method: 'POST',
--- src/modules/comments.js
+++ src/modules/comments.js
@@ -3,14 +3,15 @@ import '../style/comments.css';
 import profileImg from '../images/profile.png';
 
 const body = document.querySelector('body');
-
 const sidebar = document.createElement('div');
 sidebar.id = 'commets-sidbar';
 
+// create one comment in the sidebar
 const displayComment = (comment) => {
   const card = document.createElement('div');
   card.classList.add('card');
 
+  // a div to contain the image and the name so they can be in the same line
   const info = document.createElement('div');
   info.classList.add('info');
 
@@ -29,15 +30,19 @@ const displayComment = (comment) => {
   return card;
 };
 
+// creat all comments by using list of object from the api
 const displayComments = (comments) => {
   sidebar.innerHTML = '';
+  // check if it's null if not create all the comments an show it, if it is just show
   if (comments) {
     sidebar.classList.add('close');
+
     const close = document.createElement('span');
     close.textContent = 'X';
     close.classList.add('close');
     close.addEventListener('click', () => sidebar.classList.add('close'));
     sidebar.appendChild(close);
+
     const { length } = comments;
     for (let i = 0; i < length; i += 1) {
       sidebar.appendChild(displayComment(comments[i]));
@@ -47,6 +52,7 @@ const displayComments = (comments) => {
   sidebar.classList.remove('close');
 };
 
+// use this to make the comments shown by givin the id of the meal
 const showComments = (id) => {
   getComments(id).then((data) => displayComments(data)).catch(() => displayComments(null));
 };
